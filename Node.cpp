e2b596b3c34d190bb9d4756e0d69017ff00d85fb
6643789wsx <qaz6643789wsx@163.com>
2024-12-30 20:33:35 +0800

    feat: add more attribute and method in Node

--- Node.cpp
+++ Node.cpp
@@ -1,20 +1,52 @@
 
+#include <iostream>
 #include "Node.h"
 
 Node::Node(const std::string& label_string, const std::string& prop_string, const GPStore::Value* value) {
+    this->label_string = label_string;
 
 }
 
 Node::Node(unsigned node_id) {
-    
+    this->node_id_ = node_id;
 }
 
 GPStore::Value* Node::operator[](const std::string& property_string) {
+
 }
 
 void Node::GetLinkedNodes(const std::string& pre_str, std::shared_ptr<const unsigned[]>& nodes_list, unsigned& list_len, char edge_dir) {
+
 }
 
 void Node::GetLinkedNodesWithEdgeProps(const std::string& pre_str, std::shared_ptr<const unsigned[]>& nodes_list, std::shared_ptr<const long long[]>& prop_list,
                                        unsigned& prop_len, unsigned& list_len, char edge_dir) {
+
+}
+
+void Node::setLabel(const std::string &label_string) {
+    this->label_string = label_string;
+}
+
+void Node::setValues(const std::string &prop_string, const GPStore::Value *value) {
+    if (value != nullptr) {
+        this->columns[prop_string] = *value;
+        delete value; // 删除传入的指针
+    }
+}
+
+void Node::print() {
+    std::cout << "Node_id: " << this->node_id_ << "\n";
+    std::cout << "label_string: " << this->label_string << "\n";
+    std::cout << "columns_size: " << this->columns.size() << "\n";
+    for(auto& item:this->columns)
+        std::cout << item.first << ": " << item.second.toString() << "\n";
+    std::cout << "relations_size: " << this->relations.size() << "\n";
+    for(auto& item:this->relations)
+        std::cout << item.first << "\n";
+}
+
+void Node::addRelation(std::string relationName, std::string index, std::string prop, std::string propValue) {
+    this->relations[relationName].emplace_back(index,propValue);
+    this->relationsProp[relationName] = prop;
 }
--- Node.h
+++ Node.h
@@ -1,21 +1,57 @@
 #include <string>
 #include <memory>
 #include "Value.h"
+#include <unordered_map>
 
 class Node {
- public:
-  unsigned node_id_;
-  const std::string label_string;
-  const std::string prop_string;
+  public:
+    unsigned node_id_; // Node ID
+    std::string label_string; // Label of the node
+    std::string prop_string; // Properties of the node
+    std::unordered_map<std::string,GPStore::Value> columns;  //node的属性
 
-  Node()=default;
-  Node(const std::string& label_string, const std::string& prop_string, const GPStore::Value* value);
-  Node(unsigned node_id);
-  void Goto(unsigned new_node_id) {
-    node_id_ = new_node_id;
-  }
-  GPStore::Value* operator[](const std::string& property_string);
-  void GetLinkedNodes(const std::string&, std::shared_ptr<const unsigned[]>& nodes_list, unsigned& list_len, char edge_dir);
-  void GetLinkedNodesWithEdgeProps(const std::string& pre_str, std::shared_ptr<const unsigned[]>& nodes_list, std::shared_ptr<const long long[]>& prop_list,
-                                   unsigned& prop_len, unsigned& list_len, char edge_dir);
+    unordered_map<string, vector<pair<string, string>>> relations;
+    unordered_map<string, string> relationsProp; //The first string is the relation type, the second string is the ID
+
+    Node()=default; // Default constructor
+    Node(const std::string& label_string, const std::string& prop_string, const GPStore::Value* value); // Constructor with parameters
+    Node(unsigned node_id); // Constructor with node ID
+    void Goto(unsigned new_node_id) { // Method to update node ID
+      node_id_ = new_node_id;
+    }
+    GPStore::Value* operator[](const std::string& property_string); // Overloaded operator to access properties
+    void GetLinkedNodes(const std::string&, std::shared_ptr<const unsigned[]>& nodes_list, unsigned& list_len, char edge_dir); // Method to get linked nodes
+    void GetLinkedNodesWithEdgeProps(const std::string& pre_str, std::shared_ptr<const unsigned[]>& nodes_list, std::shared_ptr<const long long[]>& prop_list,
+                                    unsigned& prop_len, unsigned& list_len, char edge_dir); // Method to get linked nodes with edge properties
+    void setLabel(const std::string& label_string);
+    void setValues(const std::string& prop_string, const GPStore::Value* value);
+
+    void addRelation(std::string relationName, std::string index, std::string prop, std::string propValue);
+
+    void print(); // Method to print node details
+
 };
+
+// Different node lists, formatted as id2Node
+extern std::unordered_map<std::string, Node> PersonMap;
+extern std::unordered_map<std::string, Node> OrganisationMap;
+extern std::unordered_map<std::string, Node> CityMap;
+extern std::unordered_map<std::string, Node> CompanyMap;
+extern std::unordered_map<std::string, Node> PlaceMap;
+extern std::unordered_map<std::string, Node> UniversityMap;
+extern std::unordered_map<std::string, Node> CountryMap;
+extern std::unordered_map<std::string, Node> MessageMap;
+extern std::unordered_map<std::string, Node> PostMap;
+extern std::unordered_map<std::string, Node> CommentMap;
+
+// Entity ID to global ID mapping for nodes
+extern std::unordered_map<std::string, std::string> PersonIDMap;
+extern std::unordered_map<std::string, std::string> OrganisationIDMap;
+extern std::unordered_map<std::string, std::string> CityIDMap;
+extern std::unordered_map<std::string, std::string> CompanyIDMap;
+extern std::unordered_map<std::string, std::string> PlaceIDMap;
+extern std::unordered_map<std::string, std::string> UniversityIDMap;
+extern std::unordered_map<std::string, std::string> CountryIDMap;
+extern std::unordered_map<std::string, std::string> MessageIDMap;
+extern std::unordered_map<std::string, std::string> PostIDMap;
+extern std::unordered_map<std::string, std::string> CommentIDMap;
